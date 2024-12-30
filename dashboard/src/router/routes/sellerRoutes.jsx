6da180642bbc211b5b7561b6388f06a3acd17417
eldadstar <eldadfikre456@gmail.com>
2024-12-30 18:24:29 +0300

    Payment Status

--- dashboard/src/router/routes/sellerRoutes.jsx
+++ dashboard/src/router/routes/sellerRoutes.jsx
@@ -5,6 +5,9 @@ const SellerDashboard = lazy(()=> import('../../views/seller/SellerDashboard'))
 const AddProduct = lazy(()=> import('../../views/seller/AddProduct'))
 const Products = lazy(()=> import('../../views/seller/Products'))
 const DiscountProducts = lazy(()=> import('../../views/seller/DiscountProducts'))
+const Orders = lazy(()=> import('../../views/seller/Orders'))
+const Payments = lazy(()=> import("../../views/seller/Payments"))
+
 
 export const sellerRoutes = [
     {
@@ -15,21 +18,38 @@ export const sellerRoutes = [
     {
         path: '/seller/dashboard',
         element: <SellerDashboard/>,
-        ability: ['seller']
+        role: 'seller',
+        status: 'active'
+        
     },
     {
         path: '/seller/dashboard/add-product',
         element: <AddProduct/>,
-        ability: ['seller']
+        role: 'seller',
+        status: 'active'
     },
     {
         path: '/seller/dashboard/products',
         element: <Products/>,
-        ability: ['seller']
+        role: 'seller',
+        status: 'active'
     }, 
     {
         path: '/seller/dashboard/discount-product',
         element: <DiscountProducts/>,
-        ability: ['seller']
+        role: 'seller',
+        status: 'active'
+    }, 
+    {
+        path: '/seller/dashboard/orders',
+        element: <Orders/>,
+        role: 'seller',
+        ability:['active','deactive']
+    }, 
+    {
+        path: '/seller/dashboard/payments',
+        element: <Payments/>,
+        role: 'seller',
+        status: 'active'
     }, 
 ] 
